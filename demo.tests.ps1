ee6633584669126fadf406aaf8b79df373d17357
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2023-05-17 10:38:31 +0200

    test pester ansiformatted error

---
+++ demo.tests.ps1
@@ -0,0 +1,26 @@
+﻿BeforeAll {
+    function Test-EvenNumbers {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory)]
+            [int]
+            $Number
+        )
+
+        $Number % 2 -eq 0
+    }
+}
+
+Describe Test-EvenNumber {
+    Context 'Even Numbers' {
+        It '<number> is even' -TestCases @(
+            @{ Number = 1 },
+            @{ Number = 4 },
+            @{ Number = 5 }
+        ) {
+            param($Number)
+            Test-EvenNumbers -Number $Number | Should -BeTrue
+            Write-Host 'hello world'
+        }
+    }
+}
